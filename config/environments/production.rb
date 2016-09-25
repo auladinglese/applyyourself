2e636b46bac97d8b46c7c58afbf8c7b14098ae27
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2016-09-25 17:36:40 +0300

    edited mail options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.action_mailer.default_url_options = { :host => 'aydnu.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'aydnu.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'enupgrade.com' }
+  Rails.application.routes.default_url_options[:host] = 'enupgrade.com'
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -31,13 +31,13 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
+  address: "secureams30.sgcpanel.com",
+  port: 465,
   domain: ENV["GMAIL_DOMAIN"],
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  user_name: ENV["USERNAME"],
+  password: ENV["PASSWORD"]
   }  
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
